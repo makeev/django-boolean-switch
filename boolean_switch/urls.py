d1d0fdcd07f267e8167ced2b84c928d2b25ee6c0
Михаил Макеев <mihail.makeev@gmail.com>
2016-07-18 15:01:47 +0300

    remove django 1.9 warning

--- boolean_switch/urls.py
+++ boolean_switch/urls.py
@@ -1,3 +1,5 @@
+from django import get_version
+from distutils.version import StrictVersion
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -7,6 +9,11 @@ admin.autodiscover()
 
 from boolean_switch.views import switch
 
-urlpatterns = patterns('',
-    url(r'^(?P<url>.*)/switch/$', switch, name='switch'),
-)
+if StrictVersion(get_version()) > StrictVersion('1.8'):
+    urlpatterns = [
+        url(r'^(?P<url>.*)/switch/$', switch, name='switch'),
+    ]
+else:
+    urlpatterns = patterns('',
+        url(r'^(?P<url>.*)/switch/$', switch, name='switch'),
+    )
