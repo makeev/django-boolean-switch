93263df118ce88e66272d87c18b66389e3c23bcb
KHONCHAFI Zakaria <zakaria.khonchafi@gmail.com>
2022-01-03 13:20:47 +0100

    Update urls.py
    Upgraded from `url()` to `re_path()` for django 4.0

--- boolean_switch/urls.py
+++ boolean_switch/urls.py
@@ -1,19 +1,20 @@
+from boolean_switch.views import switch
 from django import get_version
 from distutils.version import StrictVersion
-from django.conf.urls import include, url
+from django.urls import include, re_path
 
 from django.contrib import admin
 admin.autodiscover()
 
-from boolean_switch.views import switch
 
 if StrictVersion(get_version()) > StrictVersion('1.8'):
     urlpatterns = [
-        url(r'^(?P<url>.*)/switch/$', switch, name='switch'),
+        re_path(r'^(?P<url>.*)/switch/$', switch, name='switch'),
     ]
 else:
     from django.conf.urls import patterns
 
     urlpatterns = patterns('',
-        url(r'^(?P<url>.*)/switch/$', switch, name='switch'),
-    )
+                           re_path(r'^(?P<url>.*)/switch/$',
+                                   switch, name='switch'),
+                           )
